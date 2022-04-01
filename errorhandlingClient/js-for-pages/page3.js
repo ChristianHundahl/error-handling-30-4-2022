a4dedbb1cb3c61fc5d1637d4cbf6ad425583101a
Christian Hundahl <chri02jx@stud.kea.dk>
2022-04-01 14:05:17 +0200

    first commit

---
+++ errorhandlingClient/js-for-pages/page3.js
@@ -0,0 +1,24 @@
+import { SERVER } from "../settings.js"
+import { handleHttpErrors, makeOptions } from "../fetchUtils.js"
+
+const API_URL = SERVER + "/api/quotes"
+
+export function setUpAddButtonHandler() {
+  document.getElementById("btn-add").onclick = addNewQuote;
+}
+
+async function addNewQuote() {
+  document.getElementById("error").innerText = ""
+  const newQuote = {};
+  newQuote.quote = document.getElementById("quote").value
+  newQuote.ref = document.getElementById("author").value
+  try {
+    const options = makeOptions("POST",newQuote)
+    const addedQuote = await fetch(API_URL,options)
+        .then(res => handleHttpErrors(res))
+    document.getElementById("addedQuote").innerText =
+        JSON.stringify(addedQuote)
+  } catch(err){
+    document.getElementById("error").innerText = err.message
+  }
+}
